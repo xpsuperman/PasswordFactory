85bfaca8bb3dd215fa4c851d12df69e83a6e4ea1
徐朋 <xupeng07@meituan.com>
2017-08-03 16:13:17 +0800

    Initial commit

---
+++ PasswordFactory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PasswordFactory
+//
+//  Created by Xu Peng on 2017/8/3.
+//  Copyright © 2017年 lilac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
